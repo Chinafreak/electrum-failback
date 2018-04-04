961de3a992b448600b37adc1a09320b0f79d7cd1
DESKTOP-2E0PR3G\Yan <yan@huelsebusch.com>
2018-04-04 20:05:44 +0200

    ..

--- app.js
+++ app.js
@@ -20,20 +20,12 @@ mkdirp(settings.databaseBackupDirectory, (err) => {
       stopBackup = true;
       exec(settings.electrumCommand + " stop");
       setTimeout(() => {
-        restore((err) => {
-          if (err) {
-            console.error("ERROR RESTORE: " + err)
-          } else {
-            console.log("Restore successful");
-            setTimeout(() => {
-              exec(settings.electrumCommand + " start");
-              setTimeout(() => {
-                stopBackup = false;
-              }, 5000);
-            }, 2000);
-          }
-        });
-      }, 10000);
+        exec("rm -rf " + settings.electrumDatabase);
+        setTimeout(() => {
+          exec(settings.electrumCommand + " start");
+          stopBackup = false;
+        }, 2000);
+      }, 15000);
     }
 
 
@@ -43,21 +35,4 @@ mkdirp(settings.databaseBackupDirectory, (err) => {
     console.error('ERROR: ', error);
   });
 
-  backup();
-  setInterval(() => {
-    backup();
-  }, settings.timeBackupMinute * 1000 * 60)
-
 });
-
-
-var backup = (callback) => {
-  if (!stopBackup) bu.backup(settings.database, settings.databaseBackupDirectory + settings.databaseBackupFile, (err) => {
-    if (err) console.error("Backup failed: " + err);
-    else console.log("Backup successful");
-  });
-}
-
-var restore = (callback) => {
-  bu.restore(settings.databaseBackupDirectory + settings.databaseBackupFile, settings.database, (err) => callback(err));
-}
